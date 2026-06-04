d510292c6f1fbea21fc802f55e2989a144df6bc4
Rejwan Islam Rizvy <rezowanislamrizvy7@gmail.com>
2026-06-04 18:37:25 +0600

    format changed

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ with col_detector:
                         
                         # Set up VideoWriter
                         # Note: mp4v is widely supported; for direct web playability, H264 is ideal
-                        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                        fourcc = cv2.VideoWriter_fourcc(*'avc1')
                         out = cv2.VideoWriter(temp_output_path, fourcc, fps, (width, height))
                         
                         progress_bar = st.progress(0.0)
